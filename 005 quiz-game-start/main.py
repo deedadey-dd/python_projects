8fe49cb593c99665e08955db5ef00127bbb248f7
Dennis Dadey <deedadey@gmail.com>
2023-08-14 16:48:45 +0000

    completed project

---
+++ 005 quiz-game-start/main.py
@@ -0,0 +1,17 @@
+from question_model import Question
+from data import question_data
+from quiz_brain import QuizBrain
+
+#question = Question()
+question_bank = []
+for questions in question_data:
+    question = Question(questions["question"], questions["correct_answer"])
+    question_bank.append(question)
+
+quiz = QuizBrain(question_bank)
+
+while quiz.still_has_questions():
+    quiz.next_question()
+else:
+    print("You've completed the quiz")
+    print(f"Your final score was: {quiz.score}/{quiz.question_number}")
