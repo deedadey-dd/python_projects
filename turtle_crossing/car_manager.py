dac823db40079b2b56d5cb238f644bb676b5a23b
Dennis Dadey <deedadey@gmail.com>
2023-09-03 00:56:40 +0000

    Create classes and get the game running

--- turtle_crossing/car_manager.py
+++ turtle_crossing/car_manager.py
@@ -9,6 +9,19 @@ MOVE_INCREMENT = 10
 class CarManager(Turtle):
     def __init__(self):
         super().__init__()
-        self.color(random.choice(COLORS))
-        self.random_y = random.randint(-)
-        self.goto()
+        self.all_cars = []
+
+    def create_car(self):
+        chance = random.randint(1, 6)
+        if chance == 5:
+            new_car = Turtle(shape="square")
+            new_car.color(random.choice(COLORS))
+            new_car.penup()
+            new_car.shapesize(stretch_len=2, stretch_wid=1)
+            new_car.setheading(180)
+            new_car.goto(300, random.randint(-12, 12) * 21)
+            self.all_cars.append(new_car)
+
+    def move_cars(self):
+        for car in self.all_cars:
+            car.forward(MOVE_INCREMENT)
--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -7,9 +7,31 @@ from scoreboard import Scoreboard
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+screen.title("Turtle Crossing")
+
+# Create Turtle Player
+player = Player()
+
+car = CarManager()
 
 game_is_on = True
+
 
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+
+    # move player upon key press
+    screen.listen()
+    screen.onkey(fun=player.move_player, key="Up")
+    screen.onkey(fun=player.reverse_player, key="Down")
+
+    car.create_car()
+    car.move_cars()
+
+    for cars in car.all_cars:
+        if player.distance(cars) < 20:
+            game_is_on = False
+            player.write("GAME OVER", move=False, font=("Courier", 25, "normal"))
+
+screen.exitonclick()
