236f6f38b9767384eca707f934930e0ec86ca181
Dennis Dadey <deedadey@gmail.com>
2023-09-18 14:03:21 +0000

    added search function and error handling

--- my_pass/main.py
+++ my_pass/main.py
@@ -2,10 +2,13 @@ from tkinter import *
 from tkinter import messagebox
 from random import choice, randint, shuffle
 import pyperclip
+import json
 
 
 FONT = ("calibri", 11, "normal")
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
+
+
 def generate_password():
     # Password Generator Project from appbrewery
 
@@ -30,10 +33,28 @@ def generate_password():
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
 
+def final_save(raw_data):
+    with open("data.json", mode="r") as file_data:
+        # Read the data file
+        data = json.load(file_data)
+        # Update the data file
+        data.update(raw_data)
+        # Write to the json file
+    with open("data.json", mode="w") as file:
+        json.dump(data, file, indent=4)
+
+
 def save():
     website = text_website.get()
     username = text_user.get()
     password = text_password.get()
+
+    new_data = {
+        website: {
+            "username": username,
+            "password": password,
+        }
+    }
 
     if len(password) < 1 or len(website) < 1:
         messagebox.askretrycancel(title="Empty Fields", message="Some Fields are Empty")
@@ -41,19 +62,52 @@ def save():
         is_correct = messagebox.askokcancel(title=f"{website} Credentials", message=f"Do you want to save? \nWebsite: {website} "
                                                                        f"\n Email/ Username: {username} "
                                                                        f"\nPassword: {password}")
-        if is_correct:
-            with open("data.txt", mode="a") as file:
-                file.write(f"{website} | {username} | {password}\n")
 
-            pyperclip.copy(f"{password}")
+        try:
+            with open("data.json", mode="r") as file_data:
+                # Read the data file
+                data = json.load(file_data)
 
-            messagebox.showinfo(title="Saved", message="Password Copied to Clipboard")
+        except FileNotFoundError:
+            with open("data.json", mode="w") as file_data:
+                # Write to the json file
+                json.dump(new_data, file_data, indent=4)
 
+        else:
+            # Update the data file
+            data.update(new_data)
+
+            with open("data.json", mode="w") as file_data:
+                # Saving updated data
+                json.dump(data, file_data, indent=4)
+
+            messagebox.showinfo(title="Saved", message="Successfully Saved")
+
+        finally:
             text_website.delete(0, END)
             # text_user.delete(0, END)
             text_password.delete(0, END)
             text_website.focus()
+# ----------------------------- SEARCH ---------------------------------#
 
+
+def search():
+    website = text_website.get()
+    try:
+        with open("data.json", mode="r") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="No Data Found", message="Save the Information First")
+
+    else:
+        try:
+            searched = data[website]
+        except KeyError:
+            messagebox.showinfo(title="Not Found", message=f"{website}, does not exist"
+                                                           f"\nWould you want to save it?")
+        else:
+            messagebox.showinfo(title=f"{website} Credentials", message=f"Username: {data[website]['username']}\n"
+                                                            f"Password: {searched['password']}")
 
 # ---------------------------- UI SETUP ------------------------------- #
 # Window
@@ -76,8 +130,8 @@ label_password = Label(text="Password:", font=FONT)
 label_password.grid(row=3, column=0)
 
 # Entries
-text_website = Entry(font=FONT, width=45)
-text_website.grid(row=1, column=1, columnspan=2)
+text_website = Entry(font=FONT, width=28)
+text_website.grid(row=1, column=1)
 text_website.focus()
 text_user = Entry(font=FONT, width=45)
 text_user.grid(row=2, column=1, columnspan=2)
@@ -86,9 +140,11 @@ text_password = Entry(font=FONT, width=28)
 text_password.grid(row=3, column=1)
 
 # Buttons
-generate_button = Button(text="Generate Password", font=("Calibri", 10, "normal"), command=generate_password)
+generate_button = Button(height=2, width=15, text="Generate Password", font=("Calibri", 10, "normal"), command=generate_password)
 generate_button.grid(row=3, column=2)
 add_button = Button(text="Add", font=FONT, width=39, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+search_button = Button(text="Search", font=FONT, width=14, height=1, command=search)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
